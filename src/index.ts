b364d0b2d037a3f5b0218b486efa362b8cdabe6f
adrien-guell <adrien.guell@hotmail.fr>
2022-10-04 16:09:38 +0200

    fix import statement

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,15 @@ import {ApiConfig} from "./ApiConfig";
 import chalk from "chalk";
 import axios from "axios";
 
-const i = `./apitester-config`;
-import(i).then((config: ApiConfig) => {
+// TODO find automaticaly the js file folder
+import(`${process.cwd()}\\lib\\apitester-config.js`).then((defaultImport) => {
+    const config: ApiConfig = defaultImport.default;
+    console.log(JSON.stringify(config));
     for (const endpoint of config.endpoints) {
         axios.get(`${config.baseUrl}${endpoint.route}`)
             .then((response) => {
                 try {
+                    console.log(response.data);
                     endpoint.decoder(response.data);
                     console.log(chalk.green(`Decoded successfully: ${endpoint.route}`));
                 } catch (e) {
