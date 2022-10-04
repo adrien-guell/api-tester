96220f1ac2b341f1a923c7ff866b76336e31bfdf
adrien-guell <adrien.guell@hotmail.fr>
2022-10-04 17:40:35 +0200

    fix import

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import {testEndpoints} from "./src/endpointTest";
-import {ApiTesterConfig} from "./lib";
+import {ApiTesterConfig} from "./src/models/ApiTesterConfig";
 export * from "./src/models/ApiTesterConfig";
 
 // TODO find automaticaly the js file folder
