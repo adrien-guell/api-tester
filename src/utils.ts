28c12c2ef6876a89d42ad0455f46836c86f2ca9e
marile.lin <marile.lin@viseo.com>
2022-10-11 15:22:50 +0200

    add test endpoint (refacto)

--- src/utils.ts
+++ src/utils.ts
@@ -5,39 +5,50 @@ import chalk from 'chalk';
 import * as fs from 'fs';
 import { readFileSync } from 'fs';
 import path from 'path';
+import { Api } from './models/Api';
+import { Endpoint } from './models/Endpoint';
+
+
+async function testEndpoint(api: Api, endpoint: Endpoint<any>, showDetails: boolean) {
+    const logFilename = `apitester_logs\\apitester-log-${Date.now()}.txt`;
+
+    let axiosRequestConfig: AxiosRequestConfig = {
+        baseURL: api.baseUrl,
+        url: endpoint.route,
+        headers: api.headers,
+        params: Object.assign({}, api.queryParameters, endpoint.queryParameters),
+    };
+
+    if (endpoint.preRequestAction)
+        axiosRequestConfig = endpoint.preRequestAction(axiosRequestConfig);
+
+    await axios
+        .request(axiosRequestConfig)
+        .then((response) => {
+            try {
+                const decodedData = endpoint.decoder(response.data);
+                if (endpoint.postRequestValidation)
+                    endpoint.postRequestValidation(decodedData, response.data);
+                console.log(chalk.green(`${endpoint.route} - Success`));
+            } catch (error) {
+                promptFail(endpoint.route, showDetails, logFilename, error);
+            }
+        })
+        .catch((error: Error | AxiosError) => {
+            promptFail(endpoint.route, showDetails, logFilename, error);
+        });
+
+    return axiosRequestConfig;
+}
 
 export async function testEndpoints(config: ApiTesterConfig, showDetails: boolean) {
     if (!fs.existsSync('apitester_logs')) fs.mkdirSync('apitester_logs');
-    const logFilename = `apitester_logs\\apitester-log-${Date.now()}.txt`;
 
     for (const api of config.apisConfig) {
         promptApiThatIsTested(api.baseUrl);
-        for (const endpoint of api.endpoints) {
-            let axiosRequestConfig: AxiosRequestConfig = {
-                baseURL: api.baseUrl,
-                url: endpoint.route,
-                headers: Object.assign({}, api.headers, endpoint.headers),
-                params: Object.assign({}, api.queryParameters, endpoint.queryParameters),
-                data: endpoint.body ?? api.body,
-            };
-            if (endpoint.preRequestAction)
-                axiosRequestConfig = endpoint.preRequestAction(axiosRequestConfig);
 
-            await axios
-                .request(axiosRequestConfig)
-                .then((response) => {
-                    try {
-                        const decodedData = endpoint.decoder(response.data);
-                        if (endpoint.postRequestValidation)
-                            endpoint.postRequestValidation(decodedData, response.data);
-                        console.log(chalk.green(`${endpoint.route} - Success`));
-                    } catch (error) {
-                        promptFail(endpoint.route, showDetails, logFilename, error);
-                    }
-                })
-                .catch((error: Error | AxiosError) => {
-                    promptFail(endpoint.route, showDetails, logFilename, error);
-                });
+        for (const endpoint of api.endpoints) {
+            testEndpoint(api, endpoint, showDetails);
         }
     }
 }
